7ea7d3f65a03a0ac2516050eb519e704c8b966c1
Leon Heuer <leon.heuer@outlook.de>
2025-04-03 00:37:26 +0200

    update gradle version and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,22 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 import java.io.FileInputStream
+import java.io.FileNotFoundException
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "2.1.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "de.skycave"
 version = "1.0.0"
-archivesName.set("Template") // TODO
 
 val localProperties = Properties()
-localProperties.load(FileInputStream(rootProject.file("local.properties")))
+try {
+    localProperties.load(rootProject.file("local.properties").inputStream())
+} catch (ignored: FileNotFoundException) {
+    logger.warn("File local.properties could not be found. Please create a file at root level specifying gpr.user and gpr.key.")
+}
 
 repositories {
     mavenCentral()
@@ -27,18 +31,39 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
-    compileOnly("de.skycave:skycavelib:2.0.0")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("de.skycave:skycavelib:2.1.1")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    archiveFileName.set("${rootProject.name}-${project.version}.jar")
+    exclude("org/**")
+    exclude("kotlin/**")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
 }
